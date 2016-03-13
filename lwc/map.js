1b15fc22c98210202112a8c2ee9beba2088a35c8
Steven Schaefer <steven.schaefer@ischool.berkeley.edu>
2016-03-13 16:41:06 -0700

    LWC commit

---
+++ lwc/map.js
@@ -0,0 +1,124 @@
+var UsMap = {
+	build : function(domRoot, onLoad) {
+
+		this.svg = d3.select(domRoot).append('div').attr('style', 'height: 100%')
+			.append("svg")
+			//.attr("width", width)
+			//.attr("height", height);
+			  .attr('width', "100%")
+			  .attr('height', '100%')
+
+		this.world = this.svg.append('g');
+
+		var this_ = this;
+
+		queue()
+			.defer(d3.json, "us.json")
+			.await(function ready(error, us) {
+				if (error) throw error;
+				console.log("drawing geometry");
+				this_.drawMapGeometry(us);
+
+				if (onLoad) {
+					onLoad();
+				}
+			});
+
+	},
+
+	drawMapGeometry : function(map) {
+
+		var world = this.world;
+
+		var width = 1200;
+		var height = 800;
+
+		var projection = d3.geo.albersUsa()
+			.scale(1280)
+			.translate([width / 2, height / 2]);
+
+		var path = d3.geo.path()
+			.projection(projection);
+
+		var this_ = this;
+
+		world.append("g")
+		  .attr("class", "counties")
+		.selectAll("path")
+		  .data(topojson.feature(map, map.objects.counties).features)
+		.enter().append("path")
+		  .attr("d", path)
+		  .on('mouseover', function(d) {
+		  	if (this_.tip) {
+				this_.tip.show(d);
+			}
+
+			this_.overCounty(d);
+		  })
+		  .on('mouseout', function(d) {
+		  	if (this_.tip) {
+				this_.tip.hide(d);
+			}
+		  });
+
+		world.append("path")
+		  .datum(topojson.mesh(map, map.objects.states, function(a, b) { return a !== b; }))
+		  .attr("class", "states")
+		  .attr("d", path);
+
+		this.addZoom();
+	},
+
+	/**
+	 * Handler should be a function accepting 1 parameter, d
+	 */
+	addTooltipHandler : function(handler) {
+		this.tip = d3.tip()
+		  .attr('class', 'd3-tip')
+		  .offset([-10, 0])
+		  .direction('n')
+		  .html(handler);
+
+		this.svg.call(this.tip);
+	},
+
+	addZoom : function() {
+		// https://bl.ocks.org/mbostock/8fadc5ac9c2a9e7c5ba2
+		var world = this.world;
+
+		var zoom = d3.behavior.zoom()
+			.scaleExtent([1, 10])
+			.on("zoom", function() {
+				world.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");  	
+			});
+
+		this.svg.call(zoom).call(zoom.event);
+	},
+
+	recolor: function(countyDataMap) {
+
+		// Not sure correct terminology... basically buckets from 0..1
+		if (this.percentiles) {
+			var percentiles = this.percentiles;
+		} else {
+			var percentiles = d3.range(0, 1, .01)
+			percentiles.push(1)
+			this.percentiles = percentiles;
+		}
+
+		var cdf = d3.scale.quantile()
+			.domain(countyDataMap.values())
+			.range(percentiles);
+
+		var colorScale = d3.scale.linear()
+			.domain([0, 1])
+			//.range([d3.rgb(237, 248, 233), d3.rgb(0, 109, 44)])
+			//.range([d3.rgb(0, 0, 0), d3.rgb(255, 0, 0)])
+			.range([d3.rgb(221, 242, 215), d3.rgb(0, 109, 44)])
+			.interpolate(d3.interpolateRgb);
+
+		d3.selectAll(".counties > path").attr('fill', function(d) {
+			return colorScale(cdf(countyDataMap.get(d.id)));
+		});
+	}
+}
