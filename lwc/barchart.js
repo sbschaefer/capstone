2ed1e8dd32d658246e64da3f13e28418dbc5de7c
Steven Schaefer <steven.schaefer@ischool.berkeley.edu>
2016-03-16 16:51:43 -0700

    Y Axis labels and model names

--- lwc/barchart.js
+++ lwc/barchart.js
@@ -12,6 +12,9 @@ function barchart(parentNode) {
   var y = d3.scale.linear()
       .range([height, 0]);
 
+  //y.domain([0, d3.max(data, function(d) { return d.value; })]);
+  y.domain([0, 15000]); //to-do: fix this
+
   var xAxis = d3.svg.axis()
       .scale(x)
       .orient("bottom");
@@ -64,8 +67,7 @@ function barchart(parentNode) {
     x.domain(data.map(function(d) { return d.id; }));
 
 
-    //y.domain([0, d3.max(data, function(d) { return d.value; })]);
-    y.domain([0, 15000]); //to-do: fix this
+
 
     //var dataById = d3.map(data, function(d) {return d.id;});
 
